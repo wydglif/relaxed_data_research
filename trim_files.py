b75cc71a2891d065d98836c11da61c885c144c6b
wydglif <wydglif.dorlus@dillard.edu>
2020-03-31 14:53:40 -0500

    Right before meeting

---
+++ trim_files.py
@@ -0,0 +1,21 @@
+# Remove comments from data files
+import os
+
+def trim_file(file, directory):
+	with open(directory+'/'+file, "r") as infile, open(directory+'/'+file[:-4]+'_new.txt','w') as outfile:
+		f_content = infile.read()
+		f_content = f_content[f_content.find(' 0 > Page'):]
+		f_content = f_content[:f_content.find('Done')]
+		outfile.write(f_content)
+		os.remove(directory+'/'+file)
+
+
+trim_file('Chip_3D_3_Block_10_RandomData_Mar28_ReadRetry_SetFeature_0.txt','chip3')
+def trim_all_files():
+	for i in range(0,5):
+		directory = 'chip'+str(i)
+		for file in os.listdir(directory):
+			print(directory, file)
+			trim_file(file, directory)
+
+#trim_all_files()
