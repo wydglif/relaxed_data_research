382268d9080d10ed3dd775b937e57a72934c0f6f
wydglif <wydglif.dorlus@dillard.edu>
2020-04-07 10:38:46 -0500

    To use powerpoint on Windows

--- trim_files.py
+++ trim_files.py
@@ -4,13 +4,13 @@ import os
 def trim_file(file, directory):
 	with open(directory+'/'+file, "r") as infile, open(directory+'/'+file[:-4]+'_new.txt','w') as outfile:
 		f_content = infile.read()
-		f_content = f_content[f_content.find(' 0 > Page'):]
+		f_content = f_content[f_content.find(' 0>  Page ='):]
 		f_content = f_content[:f_content.find('Done')]
 		outfile.write(f_content)
 		os.remove(directory+'/'+file)
 
 
-trim_file('Chip_3D_3_Block_223_RandomData_April_1_ReadRetry_SetFeature_0.txt','chip3')
+trim_file('Chip1_Block25_RandomData_WithShift_ReadRetry_SetFeature_0.txt','chip1')
 def trim_all_files():
 	for i in range(0,5):
 		directory = 'chip'+str(i)
